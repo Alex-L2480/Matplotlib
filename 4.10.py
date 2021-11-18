7ff538aa5cd38f69bb92c4dc4fc73e748b7f4d75
Alex-L2480 <trayan_07@mail.ru>
2021-11-18 23:11:13 +0300

    f

--- 4.10.py
+++ 4.10.py
@@ -8,10 +8,8 @@ ax = fig.add_subplot()
 x = np.arange(-np.pi/2,np.pi,0.1)
 ax.plot(x,np.sin(x))#SInusoida
 
-
 ax.minorticks_on()#minor grid
 ax.grid(which='major',lw=2)
 ax.grid(which='minor')
 ax.xaxis.set_minor_locator(MaxNLocator(3))#maksimum rizok dlya udobnogo prosmotra
-
 plt.show()
